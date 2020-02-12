74b74a01459edb8896785d87d383bbac207abec0
gaeremynbronyo <gaeremynbronyo@gmail.com>
2020-02-12 22:22:22 +0100

    responsiveness updated.

---
+++ JS/script-burgermenu.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function (){
+
+    var burger = document.querySelector('.burger-container'),
+       header = document.querySelector('.header'),
+       logo = document.getElementById('mobile-logo');
+       
+    
+    burger.onclick = function() {
+        header.classList.toggle('menu-opened'); 
+        logo.classList.toggle('logo-background')           
+    }
+
+    
+});
